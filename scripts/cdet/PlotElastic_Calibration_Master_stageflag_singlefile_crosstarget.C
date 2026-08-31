a821d58b2fd28bcee92fb3052434ae2a33641f7b
Ben Spaude <btspaude@wm.edu>
2026-08-31 13:46:50 -0400

    Update ECal distance in cross-target analysis

--- scripts/cdet/PlotElastic_Calibration_Master_stageflag_singlefile_crosstarget.C
+++ scripts/cdet/PlotElastic_Calibration_Master_stageflag_singlefile_crosstarget.C
@@ -67,7 +67,7 @@ static const int NumLogicalPaddlesPerSide = NumCDetPaddlesPerSide+nRef; //676
 static const int nBarsADC = 0;
 static const double ADCCUT = 150.;   //100.0
 
-static const double ECal_dist = 6.6;
+static const double ECal_dist = 6; // might be 6.6m? 8/31/2026 -BS
 static const double CDet_y_half_length = 0.30;
 
 int NXDiffBins;
