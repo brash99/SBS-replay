65746f31ba7a805fd27a2102f0ce8636733ea6c6
Edward Brash <brash@jlab.org>
2026-09-25 11:16:08 -0400

    updates

---
+++ scripts/cdet/run_lh2.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+for run in 3649 4346 4724 5295 5727; do
+  root -l -b -q "Plot_CDet_GoodPulseCandidates_AllTDC.C+(${run},\"${OUT_DIR}\",\"CDet_run${run}_good_pulse_tdc\",1.0,0.0,60.0,0.0,40.0,false,0.0,-26.0,0.020,5.0,2.0,0.0,-26.0,0.040,5.0,2.0,-10.0,10.0,3.0,4.5,true,0.51,0.08,0.0,0.17)"
+done
